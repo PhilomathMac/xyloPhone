7b59b1afe06dd51565136734498b612839458420
McKenzie Macdonald <kenziemacd@icloud.com>
2022-07-19 12:40:18 -0700

    Initial Commit

---
+++ XyloPhone/XyloPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  XyloPhoneApp.swift
+//  XyloPhone
+//
+//  Created by McKenzie Macdonald on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct XyloPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
